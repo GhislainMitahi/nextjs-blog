312d792f5e283a7c829195167762fcdbc4095964
ghislainmitahi@gmail.com <ghislainmitahi@gmail.com>
2022-12-16 17:22:43 +0200

    add getStaticProps in index

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,36 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Ghislain Mitahi, I'm a software engineer in love
-          with pair programming, with good good communication skill(soft skill) contact me <a href="https://twitter.com/GMitahi">Twitter</a></p>
-        <p>
-          Here Is my sample web site that will help me
-          to handle with Next.js and start make serious webSite in few up coming days.
-          __🙂 happy to hear that from you my friend Ghislain Mitahi, Let's grow, Free way in front of us !!!
-        </p>
+      {/* Keep the existing code here */}
+
+      {/* Add this <section> tag below the existing <section> tag */}
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
