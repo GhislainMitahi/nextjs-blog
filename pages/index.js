5b0f37b588523ddb5ec3c20e5a54a9ba1b7b18ea
ghislainmitahi@gmail.com <ghislainmitahi@gmail.com>
2022-12-16 15:59:22 +0200

    update index file

--- pages/index.js
+++ pages/index.js
@@ -10,11 +10,11 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hello, I'm Ghislain Mitahi, I'm a software engineer in love
-          with pair programming, with good good communication skill(soft skill)  </p>
+          with pair programming, with good good communication skill(soft skill) contact me <a href="https://twitter.com/GMitahi">Twitter</a></p>
         <p>
           Here Is my sample web site that will help me
           to handle with Next.js and start make serious webSite in few up coming days.
-__🙂 happy to hear that from you my friend Ghislain Mitahi, Let's grow, Free way in front of us !!!
+          __🙂 happy to hear that from you my friend Ghislain Mitahi, Let's grow, Free way in front of us !!!
         </p>
       </section>
     </Layout>
